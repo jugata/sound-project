c7d57fa400b78870b345e1d69308d5ae4520d350
Jason Sugata <jasonsugata@gmail.com>
2020-07-01 16:30:32 -0400

    refactored eventListeners minimized file by 200 lines

--- src/index.js
+++ src/index.js
@@ -71,9 +71,8 @@ droneButton.addEventListener("click", function () {
   }
 });
 
-
-
-document.getElementById("btn1").addEventListener("click", function () {
+document.getElementById("1").addEventListener("click", function () {
+  console.log("???????", this.id)
   if (!dronePlay) {
     Tone.Transport.start("+0.1");
   }
@@ -88,205 +87,27 @@ document.getElementById("btn1").addEventListener("click", function () {
   }
 });
 
-document.getElementById("btn2").addEventListener("click", function () {
-  if (!isPlaying[1]) {
-    synthCells[1].start()
-    isPlaying[1] = true;
-    this.className = "active"
-  } else {
-    synthCells[1].stop();
-    this.className = ""
-    isPlaying[1] = false;
-  }
-});
 
-document.getElementById("btn3").addEventListener("click", function () {
-  if (!isPlaying[2]) {
-    synthCells[2].start();
-    isPlaying[2] = true;
-    this.className = "active"
-  } else {
-    synthCells[2].stop();
-    this.className = ""
-    isPlaying[2] = false;
-  }
-});
+//========= PLAYER FOR EACH CELL ===========\\
 
-document.getElementById("btn4").addEventListener("click", function () {
+const player = (e) => {
+  if (e.target.id !== "drone" && e.target.id !== "stop" && e.target.id !== "1") {
+    console.log(e.target.id)
+    let id = e.target.id - 1
+    console.log("in player", e.srcElement)
+    if (!isPlaying[id]) {
+      synthCells[id].start()
+      isPlaying[id] = true;
+      e.srcElement.className = "active"
+    } else {
+      synthCells[id].stop();
+      e.srcElement.className = ""
+      isPlaying[id] = false;
+    }
+  }
+}
 
-  if (!isPlaying[3]) {
-    // Tone.Transport.start();
-    synthCells[3].start();
-    isPlaying[3] = true;
-    this.className = "active"
-  } else {
-    synthCells[3].stop();
-    this.className = ""
-    isPlaying[3] = false;
-  }
-});
-
-document.getElementById("btn5").addEventListener("click", function () {
-  if (!isPlaying[4]) {
-    synthCells[4].start();
-    isPlaying[4] = true;
-    this.className = "active"
-  } else {
-    synthCells[4].stop();
-    this.className = ""
-    isPlaying[4] = false;
-  }
-});
-
-document.getElementById("btn6").addEventListener("click", function () {
-
-  if (!isPlaying[5]) {
-    synthCells[5].start();
-    isPlaying[5] = true;
-    this.className = "active"
-  } else {
-    synthCells[5].stop();
-    this.className = ""
-    isPlaying[5] = false;
-  }
-});
-
-document.getElementById("btn7").addEventListener("click", function () {
-  if (!isPlaying[6]) {
-    synthCells[6].start();
-    isPlaying[6] = true;
-    this.className = "active"
-  } else {
-    synthCells[6].stop();
-    this.className = ""
-    isPlaying[6] = false;
-  }
-});
-
-document.getElementById("btn8").addEventListener("click", function () {
-  if (!isPlaying[7]) {
-    synthCells[7].start();
-    isPlaying[7] = true;
-    this.className = "active"
-  } else {
-    synthCells[7].stop();
-    this.className = ""
-    isPlaying[7] = false;
-  }
-});
-
-document.getElementById("btn9").addEventListener("click", function () {
-  if (!isPlaying[8]) {
-    synthCells[8].start();
-    isPlaying[8] = true;
-    this.className = "active"
-  } else {
-    synthCells[8].stop();
-    this.className = ""
-    isPlaying[8] = false;
-  }
-});
-
-document.getElementById("btn10").addEventListener("click", function () {
-  if (!isPlaying[9]) {
-    synthCells[9].start();
-    isPlaying[9] = true;
-    this.className = "active"
-  } else {
-    synthCells[9].stop();
-    this.className = ""
-    isPlaying[9] = false;
-  }
-});
-
-document.getElementById("btn11").addEventListener("click", function () {
-  if (!isPlaying[10]) {
-    synthCells[10].start();
-    isPlaying[10] = true;
-    this.className = "active"
-  } else {
-    synthCells[10].stop();
-    this.className = ""
-    isPlaying[10] = false;
-  }
-});
-
-document.getElementById("btn12").addEventListener("click", function () {
-  if (!isPlaying[11]) {
-    synthCells[11].start();
-    isPlaying[11] = true;
-    this.className = "active"
-  } else {
-    synthCells[11].stop();
-    this.className = ""
-    isPlaying[11] = false;
-  }
-});
-
-document.getElementById("btn13").addEventListener("click", function () {
-  if (!isPlaying[12]) {
-    synthCells[12].start();
-    isPlaying[12] = true;
-    this.className = "active"
-  } else {
-    synthCells[12].stop();
-    this.className = ""
-    isPlaying[12] = false;
-  }
-});
-document.getElementById("btn14").addEventListener("click", function () {
-  if (!isPlaying[13]) {
-    synthCells[13].start();
-    isPlaying[13] = true;
-    this.className = "active"
-  } else {
-    synthCells[13].stop();
-    this.className = ""
-    isPlaying[13] = false;
-  }
-});
-document.getElementById("btn15").addEventListener("click", function () {
-  if (!isPlaying[14]) {
-    synthCells[14].start();
-    isPlaying[14] = true;
-    this.className = "active"
-  } else {
-    synthCells[14].stop();
-    this.className = ""
-    isPlaying[14] = false;
-  }
-});
-document.getElementById("btn16").addEventListener("click", function () {
-  if (!isPlaying[15]) {
-    synthCells[15].start();
-    isPlaying[15] = true;
-    this.className = "active"
-  } else {
-    synthCells[15].stop();
-    this.className = ""
-    isPlaying[15] = false;
-  }
-});
-document.getElementById("btn17").addEventListener("click", function () {
-  if (!isPlaying[16]) {
-    synthCells[16].start();
-    isPlaying[16] = true;
-    this.className = "active"
-  } else {
-    synthCells[16].stop();
-    this.className = ""
-    isPlaying[16] = false;
-  }
-});
-document.getElementById("btn18").addEventListener("click", function () {
-  if (!isPlaying[17]) {
-    synthCells[17].start();
-    isPlaying[17] = true;
-    this.className = "active"
-  } else {
-    synthCells[17].stop();
-    this.className = ""
-    isPlaying[17] = false;
-  }
-});
+const buttons = document.getElementsByTagName("button")
+const buttonArr = Array.from(buttons)
+buttonArr.forEach(button => button.addEventListener("click", player));
 
